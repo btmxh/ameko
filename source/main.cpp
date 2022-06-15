925c00564af4be29dfea17b12541c2cacb5e0eca
ngoduyanh <ngoduyanh.chip@gmail.com>
2022-06-15 18:11:48 +0700

    noexcept main()

--- source/main.cpp
+++ source/main.cpp
@@ -3,10 +3,14 @@
 
 #include "lib.hpp"
 
-auto main() -> int
+auto main() noexcept -> int
 {
-  auto const lib = library {};
-  auto const message = "Hello from " + lib.name + "!";
-  std::cout << message << '\n';
-  return 0;
+  try {
+    auto const lib = library {};
+    auto const message = "Hello from " + lib.name + "!";
+    std::cout << message << '\n';
+    return 0;
+  } catch (...) {
+    return 1;
+  }
 }
